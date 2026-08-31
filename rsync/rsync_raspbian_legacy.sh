abba9d53e2d6d42688989baf2cb0cc4ea4c18957
Tomas KOVAC <office@ztk-comp.sk>
2026-08-31 13:16:10 +0200

    Create rsync_raspbian_legacy.sh

---
+++ rsync/rsync_raspbian_legacy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo `date +%F`
+rsync \
+--archive \
+--verbose \
+--progress \
+--append \
+--times \
+--links \
+--hard-links \
+--partial \
+--block-size=8192 \
+--no-owner \
+--no-group \
+--no-perms \
+--delete \
+--exclude="*favicon.ico*" \
+--exclude="*snapshotindex.txt*" \
+--exclude="*raspbian.public.key*" \
+--exclude="*multiarch*" \
+legacy.raspbian.org::raspbian/ /data1/linux/raspbian-legacy
