249c6793d103d9f8ad8b5cb8e51d025dfd1e3077
Tomas KOVAC <office@ztk-comp.sk>
2026-08-31 13:14:52 +0200

    Create rsync_debian-cloud-image.sh

---
+++ rsync/rsync_debian-cloud-image.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo `date +%F`
+rsync \
+--archive \
+--verbose \
+--progress \
+--append \
+--times \
+--links \
+--hard-links \
+--partial \
+--block-size=8192 \
+--no-owner \
+--no-group \
+--no-perms \
+--delete \
+--exclude="*favicon.ico*" \
+--exclude="*snapshotindex.txt*" \
+--exclude="*raspbian.public.key*" \
+--exclude="*multiarch*" \
+ftp.gwdg.de::pub/linux/debian/debian-cloud-image/ /data1/linux/debian-cloud-image/
